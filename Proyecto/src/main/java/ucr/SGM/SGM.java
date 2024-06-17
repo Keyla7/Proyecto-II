e467bb4905796b2dad9249df23656b7e32e2a3c7
Keyla <Keyla@DESKTOP-CM9DG8B>
2024-06-17 14:04:55 -0600

    sgm

---
+++ Proyecto/src/main/java/ucr/SGM/SGM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.SGM;
+
+/**
+ *
+ * @author Keyla
+ */
+public class SGM {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+    }
+}
